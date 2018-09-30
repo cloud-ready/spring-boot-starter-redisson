7368507cb3975d76a486ca64020ffe9c0338c374
haolun zhang <opensource@infra.top>
2018-09-30 17:15:14 +0800

    update build scripts

--- src/test/java/top/infra/cloudready/test/RedisSentinelTests.java
+++ src/test/java/top/infra/cloudready/test/RedisSentinelTests.java
@@ -1,6 +1,7 @@
 package top.infra.cloudready.test;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.junit.Assert.assertEquals;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,53 +25,64 @@ import java.net.InetAddress;
 public class RedisSentinelTests {
 
     static final String REDIS_IMAGE = "cloudready/redis:3.0.6";
-    private static final String LOCALHOST_STR = getLocalHostQuietly();
+
     @ClassRule
     public static final GenericContainer redisMaster = new GenericContainer(REDIS_IMAGE)
         .withCommand("redis-server", "--port", "6379")
         .withExposedPorts(6379)
-        .withExtraHost("redis-sentinel", LOCALHOST_STR)
-        .withExtraHost("redis-slave", LOCALHOST_STR);
+        .withExtraHost("redis-sentinel", getLocalHostQuietly())
+        .withExtraHost("redis-slave", getLocalHostQuietly());
+
     @ClassRule
     public static final GenericContainer redisSentinel = new GenericContainer(REDIS_IMAGE)
         .withCommand("redis-server", "/etc/redis/sentinel.conf", "--sentinel")
-        .withEnv("SENTINEL_MASTER_HOSTORIP", LOCALHOST_STR)
+        .withEnv("SENTINEL_MASTER_HOSTORIP", getLocalHostQuietly())
         .withEnv("SENTINEL_MASTER_NAME", "mymaster")
         .withEnv("SENTINEL_MASTER_PORT", "6379")
         .withEnv("SENTINEL_PORT", "26379")
         .withExposedPorts(26379)
-        .withExtraHost("redis-master", LOCALHOST_STR)
-        .withExtraHost("redis-slave", LOCALHOST_STR);
+        .withExtraHost("redis-master", getLocalHostQuietly())
+        .withExtraHost("redis-slave", getLocalHostQuietly());
+
     @ClassRule
     public static final GenericContainer redisSlave = new GenericContainer(REDIS_IMAGE)
-        .withCommand("redis-server", "--slaveof", LOCALHOST_STR, "6379", "--port", "6381")
+        .withCommand("redis-server", "--slaveof", getLocalHostQuietly(), "6379", "--port", "6381")
         .withExposedPorts(6381)
-        .withExtraHost("redis-master", LOCALHOST_STR)
-        .withExtraHost("redis-sentinel", LOCALHOST_STR);
+        .withExtraHost("redis-master", getLocalHostQuietly())
+        .withExtraHost("redis-sentinel", getLocalHostQuietly());
 
     static {
-        log.info("LOCALHOST_STR: {}", LOCALHOST_STR);
+        log.info("LOCALHOST_STR: {}", getLocalHostQuietly());
         redisMaster.setPortBindings(newArrayList("6379:6379"));
         redisSentinel.setPortBindings(newArrayList("26379:26379"));
         redisSlave.setPortBindings(newArrayList("6381:6381"));
 
+        final String envSpringRedisHost = System.getenv("SPRING_REDIS_HOST");
+
+        final String redisHost = isNotBlank(envSpringRedisHost) ? envSpringRedisHost : getLocalHostQuietly();
         System.setProperty("spring.redis.sentinel.master", "mymaster");
-        System.setProperty("spring.redis.sentinel.nodes", "127.0.0.1:26379");
+        System.setProperty("spring.redis.sentinel.nodes", redisHost + ":26379");
+    }
+
+    public static String getLocalHostQuietly() {
+        String localAddress;
+
+        final String envHostIpaddress = System.getenv("HOST_IPADDRESS");
+        if (isNotBlank(envHostIpaddress)) {
+            localAddress = envHostIpaddress;
+        } else {
+            try {
+                localAddress = InetAddress.getLocalHost().getHostAddress();
+            } catch (Exception ex) {
+                log.info("getLocalHostQuietly", "cant resolve localhost address", ex);
+                localAddress = "127.0.0.1";
+            }
+        }
+        return localAddress;
     }
 
     @Autowired
     private RedissonClient redissonClient;
-
-    public static String getLocalHostQuietly() {
-        String localAddress;
-        try {
-            localAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (Exception ex) {
-            log.info("getLocalHostQuietly", "cant resolve localhost address", ex);
-            localAddress = "localhost";
-        }
-        return localAddress;
-    }
 
     @Test
     public void testRedisson() {
